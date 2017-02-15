d500fe3135a448d7bac30627e30a0b7bb74c74da
Alexander Paterson <alex1234@live.com.au>
2017-02-15 17:29:09 +0800

    INITIAL COMMIT

---
+++ README.md
@@ -0,0 +1,15 @@
+# Insert Australian GeoNames Suburbs Into Redis
+
+This script inserts a list of Australian suburbs from GeoNames into a Redis database.
+
+# How To
+
+1. Download http://download.geonames.org/export/zip/AU.zip
+
+2. Unzip to get AU.txt. Place this in the root of this package.
+
+3. Run index.js with: `node index.js --file="AU.txt" --server="redis://127.0.0.1:6379" --key="aus_suburbs"`
+
+# Confirmation
+
+Get list `LRANGE aus_suburbs 0 -1`.
---
+++ index.js
@@ -0,0 +1,40 @@
+var fs = require('fs'),
+    path = require('path'),
+    redis = require('redis'),
+    argv = require('minimist')(process.argv);
+
+var {file, server, key} = argv;
+file = file || path.join(__dirname, 'AU.txt');
+server = server || `redis://127.0.0.1:6379`;
+key = key || `aus_suburbs`;
+
+var client = redis.createClient(server);
+
+
+exports.setupClient = function() {
+  return redis.createClient();
+};
+
+var suburbs = new Set();
+
+fs.readFile(file, 'ascii', (err, data) => {
+  if (err) throw err;
+  client.del(key, err => {
+    if (err) throw err;
+    if (!data) throw new Error('Could not find data file.');
+    data.split('\n').forEach(line => {
+      var suburb = line.split('\t')[2];
+      if (suburb) {
+        suburb = suburb.toLowerCase();
+        suburbs.add(suburb);
+      }
+    });
+    var array = [];
+    suburbs.forEach(suburb => array.push(suburb));
+    client.rpush(key, array, err => {
+      if (err) throw err;
+      console.log('Success!');
+      client.quit();
+    });
+  });
+});
